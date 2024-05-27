33f03b6dd37a9b5f6a057c370985de79e79a4880
batoszwalicki <walicki.bartosz@gmail.com>
2024-05-27 22:52:08 +0200

    Stub generate thumbnails file.

--- src/app/features/panorama/components/panorama-thumbnail/panorama-thumbnail.component.ts
+++ src/app/features/panorama/components/panorama-thumbnail/panorama-thumbnail.component.ts
@@ -1,11 +1,11 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { PanoramaItem } from '../../panoramas-static-source/models/panorama-item';
 
 @Component({
   selector: 'app-panorama-thumbnail',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, NgOptimizedImage],
   templateUrl: './panorama-thumbnail.component.html',
   styleUrl: './panorama-thumbnail.component.scss',
 })
